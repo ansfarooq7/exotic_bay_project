1045e0fa7a1e04b879765062a65e1da5fea858d1
Ans Farooq <2390370f@student.gla.ac.uk>
2020-03-02 17:11:45 +0000

    Created admin interface

--- exotic_bay/views.py
+++ exotic_bay/views.py
@@ -1,29 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-	context_dict = {}
+    context_dict = {}
 
     # Obtain our Response object early so we can add cookie information.
-	response = render(request, 'exotic_bay/index.html', context=context_dict)
+    response = render(request, 'exotic_bay/index.html', context=context_dict)
 
-	# Render the response and send it back.
-	return response
+    # Render the response and send it back.
+    return response
+
 
 def contact_us(request):
-	context_dict = {}
+    context_dict = {}
 
     # Obtain our Response object early so we can add cookie information.
-	response = render(request, 'exotic_bay/contact_us.html', context=context_dict)
+    response = render(request, 'exotic_bay/contact_us.html', context=context_dict)
 
-	# Render the response and send it back.
-	return response
+    # Render the response and send it back.
+    return response
+
 
 def about(request):
-	context_dict = {}
-	
-    # Obtain our Response object early so we can add cookie information.
-	response = render(request, 'exotic_bay/about.html', context=context_dict)
+    context_dict = {}
 
-	# Render the response and send it back.
-	return response
+    # Obtain our Response object early so we can add cookie information.
+    response = render(request, 'exotic_bay/about.html', context=context_dict)
+
+    # Render the response and send it back.
+    return response
