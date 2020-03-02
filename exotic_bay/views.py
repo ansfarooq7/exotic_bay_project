48a2e220ae82447cf6c703ee988f6c4230f0400a
Tom Anderson <2384693a@student.gla.ac.uk>
2020-03-02 14:15:43 +0000

    About page link and view

--- exotic_bay/views.py
+++ exotic_bay/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
 def index(request):
     return HttpResponse("Exotic-Bay homepage")
+
+def about(request):
+	return HttpResponse("This is the about page")
