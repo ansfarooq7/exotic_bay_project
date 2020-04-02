2b7a3ddc8f78acbb445431dcf328ed4454dc6f90
Stephen Graham <2385583g@student.gla.ac.uk>
2020-04-02 16:48:09 +0100

    Added remove pet button to the basket

--- exotic_bay/views.py
+++ exotic_bay/views.py
@@ -168,6 +168,36 @@ def success(request):
     return response
 
 
+@login_required
+def remove_pet_from_basket(request, slug):
+    pet = get_object_or_404(Pet, slug=slug)
+    basket_qs = Basket.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if basket_qs.exists():
+        basket = basket_qs[0]
+        # check if the pet order is in the basket
+        if basket.pets.filter(pet__slug=pet.slug).exists():
+            pet_order = PetOrder.objects.filter(
+                pet=pet,
+                user=request.user,
+                ordered=False
+            )[0]
+            if pet_order.quantity >= 1:
+                basket.pets.remove(pet_order)
+            messages.info(request, "This pet quantity was updated.")
+            return redirect("exotic_bay:basket")
+        else:
+            messages.info(request, "This pet was not in your basket")
+            return redirect("exotic_bay:pet_details", type=pet.type, slug=slug)
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("exotic_bay:pet_details", type=pet.type, slug=slug)
+
+
+
+
 def basket(request):
     try:
         pets = []
